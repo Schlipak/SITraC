b18288d7c255eda519ba3641e1c343509f3e88a9
Guillaume de Matos guillaume.de-matos@epitech.eu <guillaume.de-matos@epitech.eu>
2019-03-31 18:38:22 +0200

    Updates

---
+++ precache-manifest.246bf08889a881fd9097564061534f86.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f9957bdb4d08fb4e81b6a8566536df5",
+    "url": "/sitrac/static/media/croix_occitane.3f9957bd.svg"
+  },
+  {
+    "revision": "a6b827b7e7d91ec6f1ddace434594809",
+    "url": "/sitrac/static/media/cemetery.a6b827b7.svg"
+  },
+  {
+    "revision": "7f876071e951bec03e861227d870036b",
+    "url": "/sitrac/static/media/Frutiger_bold.7f876071.woff"
+  },
+  {
+    "revision": "25d86d63a5938d25b1ff8ffa48496954",
+    "url": "/sitrac/static/media/Frutiger.25d86d63.woff"
+  },
+  {
+    "revision": "6699ef186fce9b646f15",
+    "url": "/sitrac/static/js/runtime~main.6af847eb.js"
+  },
+  {
+    "revision": "04dc48740bc448219163",
+    "url": "/sitrac/static/js/main.51482679.chunk.js"
+  },
+  {
+    "revision": "1e5c94a3ec7ecb885c61",
+    "url": "/sitrac/static/js/2.1bb37c49.chunk.js"
+  },
+  {
+    "revision": "04dc48740bc448219163",
+    "url": "/sitrac/static/css/main.ed19ba26.chunk.css"
+  },
+  {
+    "revision": "5cf7f915b88572f2cbab8263dc47dc0f",
+    "url": "/sitrac/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ab1dbdf07d37fa556e63016db49826cf.js"
+  "/sitrac/precache-manifest.246bf08889a881fd9097564061534f86.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/sitrac/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
