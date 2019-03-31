af3977885a9018b0505b3122a2548265a8065f25
Guillaume de Matos guillaume.de-matos@epitech.eu <guillaume.de-matos@epitech.eu>
2019-03-31 18:32:40 +0200

    Updates

---
+++ precache-manifest.ab1dbdf07d37fa556e63016db49826cf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f9957bdb4d08fb4e81b6a8566536df5",
+    "url": "/static/media/croix_occitane.3f9957bd.svg"
+  },
+  {
+    "revision": "a6b827b7e7d91ec6f1ddace434594809",
+    "url": "/static/media/cemetery.a6b827b7.svg"
+  },
+  {
+    "revision": "7f876071e951bec03e861227d870036b",
+    "url": "/static/media/Frutiger_bold.7f876071.woff"
+  },
+  {
+    "revision": "25d86d63a5938d25b1ff8ffa48496954",
+    "url": "/static/media/Frutiger.25d86d63.woff"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "578b8afe46d8240bacec",
+    "url": "/static/js/main.872e98fa.chunk.js"
+  },
+  {
+    "revision": "411d9bb0f37465bfe7cd",
+    "url": "/static/js/2.997e7b40.chunk.js"
+  },
+  {
+    "revision": "578b8afe46d8240bacec",
+    "url": "/static/css/main.68c5e2fc.chunk.css"
+  },
+  {
+    "revision": "e7b62609524ccf98681bde38007ce43b",
+    "url": "/index.html"
+  }
+];
